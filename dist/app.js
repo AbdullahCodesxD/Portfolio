1005174ea59230cc3f9f451b66a945daed2390bf
codingwebthree <99201977+codingwebthree@users.noreply.github.com>
2022-09-28 11:43:41 +0500

    Add files via upload

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const observor = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+    })
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach(el=>observor.observe(el));
